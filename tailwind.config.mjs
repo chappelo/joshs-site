f9210019753e1c6a75aaabffcbd26784511a59d2
Josh Chappelow <jdchappelow@gmail.com>
2024-11-03 23:42:03 +0900

    wip: redesign

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 
-import defaultTheme from 'tailwindcss/defaultTheme'
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     colors: {
       primary: "#606676",
@@ -13,16 +13,29 @@ export default {
       effect: "#399918",
       // white: "#F6FFF8",
       white: "#ffffff",
+      black: "#222222",
     },
     screens: {
       laptop: "717px",
     },
     extend: {
-            fontFamily: {
-        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
       },
-    }
+      backgroundImage: {
+        main: "url('/noise.png')",
+      },
+      keyframes: {
+        noise: {
+          "0%": { backgroundPosition: "0 0" },
+          "100%": { backgroundPosition: "123px 456px" },
+        },
+      },
+      animation: {
+        noise: "noise 1s steps(13) infinite",
+      },
+    },
   },
   plugins: [],
   darkMode: ["selector", '[data-theme="dark"]'],
-}
+};
