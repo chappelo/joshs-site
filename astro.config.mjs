edadffa59f5ed6045d44297a1926f5d75ac48557
Josh Chappelow <jdchappelow@gmail.com>
2026-05-19 15:10:51 +0900

    fix: getStaticPaths at build, audit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
-import { defineConfig, passthroughImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  output: "server",
-  image: {
-    service: passthroughImageService(),
-  },
-  adapter: vercel(),
+  output: "static",
 });
